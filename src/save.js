a27932f40e4d1467a1e3e9ed5bc7efea978b0953
stephanepiette <piettes2010@gmail.com>
2022-12-07 18:12:26 +0100

    design card

--- src/save.js
+++ src/save.js
@@ -16,10 +16,38 @@ import {useBlockProps, RichText} from '@wordpress/block-editor';
  * @return {WPElement} Element to render.
  */
 export default function save({attributes}) {
+
 	return (
 		<div {...useBlockProps.save()} >
 
-			<div className="grid md:grid-cols-3">
+			<div className="grid lg:grid-cols-2 gap-2">
+
+
+				<div className={"player-name-image " + (attributes.right ? "lg:order-last" : "")}>
+					<div>
+						<img className="player-image" src={attributes.mediaUrl}/>
+					</div>
+					<div className="player-name inline-block text-5xl lg:text-5xl px-4 py-4">
+						Body
+						Bab
+					</div>
+				</div>
+
+				<div className={"flex flex-col justify-center player-info"}>
+
+					<div className="player-description">
+						Grosse description avec un super text de la mort qui tue. Et après on est content et on va jouer
+						aux
+						billes
+					</div>
+					<div className="grid grid-cols-2 player-attributes">
+						<div>Taille</div>
+						<div>1400 mm</div>
+						<div>Ville de naissance</div>
+						<div>Loutreville</div>
+						<div>🇩🇪🇬🇧🇫🇷🇨🇦</div>
+					</div>
+				</div>
 
 				{/*<div>*/}
 				{/*	<div>*/}
@@ -44,32 +72,8 @@ export default function save({attributes}) {
 				{/*	<div>{attributes.val3}</div>*/}
 				{/*</div>*/}
 
-				<div className={"player-name-image " + (attributes.right ? "md:order-last" : "")}>
-					<div>
-						<img className="player-image" src={attributes.mediaUrl}/>
-					</div>
-					<h2 className="player-name">{attributes.name}</h2>
 
-
-				</div>
-
-				<div className={"md:col-span-2 flex flex-col justify-center player-info"}>
-
-					<div className="player-description">
-						Grosse description avec un super text de la mort qui tue. Et après on est content et on va jouer
-						aux
-						billes
-					</div>
-					<div className="grid grid-cols-2 player-attributes">
-						<div>Taille</div>
-						<div>1400 mm</div>
-						<div>Ville de naissance</div>
-						<div>Loutreville</div>
-						<div>🇩🇪🇬🇧🇫🇷🇨🇦</div>
-					</div>
-				</div>
 			</div>
-
 		</div>
 	);
 }
